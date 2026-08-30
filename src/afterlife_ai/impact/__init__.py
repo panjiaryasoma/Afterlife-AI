af5548fbff8d0633243ee614d2a9dfdc14263273
Panji Arya Soma <214255890+panjiaryasoma@users.noreply.github.com>
2026-08-30 23:08:17 +0700

    feat: export report sustainability adapter

--- src/afterlife_ai/impact/__init__.py
+++ src/afterlife_ai/impact/__init__.py
@@ -2,10 +2,12 @@
 
 from .batch import build_batch_sustainability_summary
 from .reconciliation import reconcile_outcome
+from .reporting import build_report_sustainability_summary
 from .summary import build_sustainability_summary
 
 __all__ = [
     "build_batch_sustainability_summary",
+    "build_report_sustainability_summary",
     "build_sustainability_summary",
     "reconcile_outcome",
 ]
